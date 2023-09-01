dcf4a42cca610166d151eccf4fafb53958a18396
Matee <matiullahsafibbc@gmail.com>
2023-09-01 18:51:51 +0430

    Add isPending method

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import * as styles from '../styles/contactform.module.css';
+import '../styles/contactform.module.css';
+import Check from '../../static/check.png';
 
 const ContactForm = ({ buttonLabel }) => {
   const [isFormVisible, setFormVisibility] = useState(false);
   const [isSuccess, setSuccess] = useState(false);
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsPending(true);
     const formData = new FormData(e.target);
     const response = await fetch('https://formspree.io/f/mwkdozyv', {
       method: 'POST',
@@ -19,14 +23,14 @@ const ContactForm = ({ buttonLabel }) => {
       setFormVisibility(false);
       setSuccess(true);
     }
+    setIsPending(false);
   };
 
   return (
     <div className='popup-container'>
-      <button className="email-btn" onClick={() => setFormVisibility(true)}>
-        {buttonLabel}
+      <button className={`email-btn ${isSuccess ? 'success' : ''}`} onClick={() => setFormVisibility(true)}>
+        {isSuccess ? <img src={Check} width={50} alt="success check" /> : buttonLabel}
       </button>
-
       {isFormVisible && (
         <div className={styles.popup}>
           <div className={styles.popupContent}>
@@ -37,13 +41,14 @@ const ContactForm = ({ buttonLabel }) => {
             <form onSubmit={handleSubmit} className={styles.form}>
               <input type="text" name="name" placeholder="Your Name" required />
               <input type="email" name="email" placeholder="Your Email" required />
-              <textarea name="message" placeholder="Your Message" rows={10} required />
-              <button type="submit" className={styles.sendButton}>Send</button>
+              <textarea name="message" placeholder="Your Message" rows={5} required />
+              <button type="submit" className={styles.sendButton}>
+                {isPending ? 'Submitting...' : 'Send'}
+              </button>
             </form>
           </div>
         </div>
       )}
-      {isSuccess && <p>Message sent successfully!</p>}
     </div>
   );
 };
